5cf62378a6bb909a377ef30d58cff0bbfc72ed48
Chris Walton <chrisw@apadmi.com>
2021-01-15 15:08:38 +0000

    ReactBlog

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(res => {
+  console.log('Response', res);
+  return res;
+}, error => {
+  console.log('Error', error);
+  return Promise.reject(error);
+});
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
